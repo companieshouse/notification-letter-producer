97aa75bd3506b170f01a34b611c22fedc7291215
ehenry27 <ehenry@companieshouse.gov.uk>
2025-04-30 16:49:16 +0100

    Scripts and logging

--- scripts/notification-letter-producer.sh
+++ scripts/notification-letter-producer.sh
@@ -2,22 +2,40 @@
 
 d0=$0 # Record script name as it is lost when calling functions
 
+# set up logging
+LOGS_DIR=../logs/notification-letter-producer
+mkdir -p ${LOGS_DIR}
+LOG_FILE="${LOGS_DIR}/${HOSTNAME}-notification-letter-producer-$(date +'%Y-%m-%d_%H-%M-%S').log"
+source logging_functions
+
 function main_f {
-  printf "start notification-letter-producer: %s\n" "$(date)"
+
+exec > >(tee "${LOG_FILE}") 2>&1
+  f_logInfo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  f_logInfo "Starting notification-letter-producer"
+  f_logInfo "start notification-letter-producer: %s\n" "$(date)"
 
   ## set up Global Env Standalone specific variables like PATH, global CLASSPATH, connections, alerts across jobs, etc.
   if [[ -e $HOME/standalone/standalone.properties ]]; then
     . $HOME/standalone/standalone.properties
   fi
 
-  java -jar notification-letter-producer.jar notification-letter-producer.properties
+f_logInfo "Checking that notification-letter-producer is not already running"
+../scripts/check-for-jms-message.sh ${JMS_LETTERPRODUCERQUEUE}
+if [ $? -gt 0 ]; then
+        f_logError "notification-letter-producer may be already running or connection error.  Please investigate."
+        patrol_log_alert_chaps_f " `pwd`/`basename $0`:  notification-letter-producer may be already running or connection error.  Please investigate."
+        exit 1
+fi
 
   if [ $? -gt 0 ]; then
-    printf "Non-zero exit code for notification-letter-producer java execution\n"
+    f_logError "Non-zero exit code for notification-letter-producer java execution\n"
+      f_logInfo "ending notification-letter-producer: %s\n" "$(date)"
+          f_logInfo "Ending letter-producer"
+          f_logInfo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     exit 1
   fi
 
-  printf "ending notification-letter-producer: %s\n" "$(date)"
 }
 
 cd ${0%/*} # cd into directory to run
